65a96b332aa6e75dbe8e660d11ace665e51005d7
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-05-26 22:18:12 -0500

    controlando un error de directorio ya creado

--- creandoCarpeta.py
+++ creandoCarpeta.py
@@ -1,4 +1,20 @@
-import os 
+"""1ra forma"""
+# import os 
 
-os.mkdir("example_directory/")
+# os.mkdir("example_directory/")
+"""2da forma"""
+from pathlib import Path 
+# Creamos un objeto "obj" de la clase Path y le pasamos como argumento la ruta
+obj= Path("example2_directory/")
+# Accedemos a su método mkdir()
+try:
+    obj.mkdir()
+except FileExistsError as e:
+    # Nos mostrará un error más entendible
+    print(e)
+# ########################
+
+"""Si la ruta ya existe nos generará un Error -> FileExistsError"""
+# Lo controlamos con un Try Except
+
 
